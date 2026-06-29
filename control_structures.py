9b17e79233796988fe4fac245f424b65cc5fa26b
Gees1902 <59318940+Gees1902@users.noreply.github.com>
2026-06-28 22:35:18 -0500

    update lists.py

---
+++ control_structures.py
@@ -0,0 +1,20 @@
+# Control Structures / Control Flow
+# instance_running = "broken"
+# If Statements
+# if instance_running == "running":
+#    print("The ec2 is running")            
+# elif instance_running == "stopped":
+#    print("the ec2 is stopped....")
+# else:
+#    print("the ec2 instance is in an unexpect state")
+
+
+Public_access_block = False
+
+# Write the if statement 
+# if condition
+if Public_access_block == True:
+    print('s3 bucket is secured')
+else:
+    print("S3 bucket is not secure")
+
---
+++ forloops.py
@@ -0,0 +1,11 @@
+# For Loops
+
+instance_ids = ["i-123456abc", "i-23456", "i-abcde1234"]
+
+for instance_id in instance_ids:
+    print(f"Checking status of instance {instance_id}")
+    # code to check instancce status
+    print(f"instance {instance_id} status check complete")
+
+print("All instance have been checked")
+
